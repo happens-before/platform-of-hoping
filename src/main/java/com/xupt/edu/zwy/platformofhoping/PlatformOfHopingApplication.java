2f3de8ee04632c039c7624145de02f8264d3f768
wanyue.zhao <wanyue.zhao@qunar.com>
2019-02-18 15:48:57 +0800

    种希望大学生公益平台环境搭建

---
+++ src/main/java/com/xupt/edu/zwy/platformofhoping/PlatformOfHopingApplication.java
@@ -0,0 +1,13 @@
+package com.xupt.edu.zwy.platformofhoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformOfHopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformOfHopingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xupt/edu/zwy/platformofhoping/PlatformOfHopingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xupt.edu.zwy.platformofhoping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlatformOfHopingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
